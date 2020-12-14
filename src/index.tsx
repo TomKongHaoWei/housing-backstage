b9f4ddd82d8ee70d75708ad21ae83dd71fc64ac8
KongHaoWei <583039071@qq.com>
2020-12-14 15:16:21 +0800

    fix：路由与ts问题

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route path="/" component={App} exact />
+    </Switch>
+  </Router>,
   document.getElementById('root')
-);
+)
 
 reportWebVitals();
